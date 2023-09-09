3c621c9e7f2a89aa76b55b1b4315079c2db46272
GopalAror <punjabijanu586@gmail.com>
2023-09-09 11:17:31 +0530

    changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,11 @@
 let mobileView = document.querySelector(".mobileView")
 let menu = document.querySelector("#menu")
+let home =document.querySelector(".home")
+let srvice =document.querySelector(".srvice")
+let product =document.querySelector(".product")
+let about =document.querySelector(".about")
 
 menu.addEventListener("click",function(){
-  mobileView.classList.toggle("show")
+  mobileView.classList.toggle("show");
+  document.querySelector("body").classList.toggle("overflow-hidden")
 })
