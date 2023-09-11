823b3fdc115e645c80823ff21dd45695293678c7
GopalAror <punjabijanu586@gmail.com>
2023-09-11 10:57:21 +0530

    html changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,5 @@
 let mobileView = document.querySelector(".mobileView")
 let menu = document.querySelector("#menu")
-let home =document.querySelector(".home")
-let srvice =document.querySelector(".srvice")
-let product =document.querySelector(".product")
-let about =document.querySelector(".about")
 
 menu.addEventListener("click",function(){
   mobileView.classList.toggle("show");
